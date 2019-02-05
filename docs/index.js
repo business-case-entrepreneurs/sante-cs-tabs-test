9777405a7d4b4da52814f52c80a9c284b232b0f1
Rick Koster <ngf.koster@outlook.com>
2019-02-05 16:45:06 +0100

    test script

---
+++ docs/index.js
@@ -0,0 +1,19 @@
+function openProcessWindow(url, id) {
+  const event = new CustomEvent('spe:open', { detail: { url, id } });
+  document.dispatchEvent(event);
+}
+
+function closeProcessWindow(id) {
+  const event = new CustomEvent('spe:close', { detail: { id } });
+  document.dispatchEvent(event);
+}
+
+document.addEventListener('spe:closed', event => {
+  const { id } = event.detail;
+  console.log('closed ' + id);
+});
+
+document.addEventListener('spe:select', event => {
+  const { id } = event.detail;
+  console.log('select ' + id);
+});
